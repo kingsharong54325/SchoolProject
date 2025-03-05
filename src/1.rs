7302d7b2a90150eb86e80e5a53b4b797322c00cb
kingsharong54325 <king_sharong54325@gmx.com>
2025-03-05 19:28:13 +0000

    Add 1.rs

---
+++ src/1.rs
@@ -0,0 +1,8 @@
+// Generating a random Rust code snippet
+let mut rng = rand::thread_rng();
+
+// Get a random number between 1 and 5
+let num = rng.gen_range(1..=5);
+
+// Print the number to the console
+println!("Random number: {}", num);
